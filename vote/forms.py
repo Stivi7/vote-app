cf4af8887e67d2a502f879a25f614c9bffda3430
Stivi7 <stivi.ndoni@gmail.com>
2017-11-29 14:53:53 +0100

    bootstrap forms

--- vote/forms.py
+++ vote/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.auth.models import User
+
 
 
 class PollForm(forms.Form):
@@ -18,9 +21,60 @@ class PollForm(forms.Form):
             raise forms.ValidationError('You have to write something!')
 
 
-class UserCreateForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super(UserCreateForm, self).__init__(*args, **kwargs)
+# class UserCreateForm(UserCreationForm):
+#     def __init__(self, *args, **kwargs):
+#         super(UserCreateForm, self).__init__(*args, **kwargs)
 
-        for fieldname in ['username', 'password1', 'password2']:
-            self.fields[fieldname].help_text = None
+#         for fieldname in ['username', 'password1', 'password2']:
+#             self.fields[fieldname].help_text = None
+
+class UserCreateForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+
+    username = forms.CharField(label=_("Username"),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username'
+        })
+        )
+    
+    password1 = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+        }))
+    password2 = forms.CharField(label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm Password',
+        }))
+
+    class Meta:
+        model = User
+        fields = ("username",)
+
+        
+
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
